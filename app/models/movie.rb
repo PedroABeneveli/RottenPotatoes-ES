0eddb6e2c53cf2ba8cb4f0fdfb777856d19efd3c
Pedro Beneveli <pedro.beneveli@gmail.com>
2024-04-26 15:44:12 -0300

    Model, migration e DB

---
+++ app/models/movie.rb
@@ -0,0 +1,3 @@
+# por enquanto eh o basico necessario para um model, herdando do active records
+class Movie < ActiveRecord::Base
+end
---
+++ db/migrate/20240426180528_create_movies_table.rb
@@ -0,0 +1,14 @@
+class CreateMoviesTable < ActiveRecord::Migration[7.1]
+  def change
+    create_table :movies_tables do |t|
+      # nome das colunas da tabela
+      t.string 'title'
+      t.string 'rating'
+      t.text 'description'
+      t.datetime 'release_date'
+
+      # timestamps deixam o rails rastrear quando um dado eh adicionado ou modificado
+      t.timestamps
+    end
+  end
+end
